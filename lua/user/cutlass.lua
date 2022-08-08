b0de9b500a76cf9a627e2a2d3b19f16daf6b3a8d
Santiago Garcia <santiago.garcia@posteo.de>
2022-08-08 22:36:24 +0200

    Favorite plugins

--- lua/user/cutlass.lua
+++ lua/user/cutlass.lua
@@ -1,11 +1,10 @@
-
 local status_ok, cutlass = pcall(require, "cutlass")
 if not status_ok then
-  return
+	return
 end
 
-cutlass.setup ({
-    cut_key = "x",
-    override_del = true,
-    exclude = {}
+cutlass.setup({
+	cut_key = nil,
+	override_del = nil,
+	exclude = { "ns", "nS" },
 })
