582ec24e38bc5336f69f974f01eada230e7fc266
Santiago Garcia <santiago.garcia@posteo.de>
2022-08-13 13:28:00 +0200

    Disable parameter icon

--- lua/user/signature.lua
+++ lua/user/signature.lua
@@ -32,7 +32,7 @@ signature.setup({
 	close_timeout = 4000, -- close floating window after ms when laster parameter is entered
 	fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
 	hint_enable = true, -- virtual hint enable
-	hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+	hint_prefix = "", -- Parameter icon, NOTE: for the terminal not support emoji, might crash
 	hint_scheme = "String",
 	hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
 	handler_opts = {
