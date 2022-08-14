1b558aa62343ec7cdde8b4e76d6279de8b67cc99
Santiago Garcia <santiago.garcia@posteo.de>
2022-08-14 10:26:16 +0200

    Improve bufferline settings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -29,8 +29,10 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<leader>bn", ":bnext<CR>", opts)
-keymap("n", "<leader>bp", ":bprevious<CR>", opts)
+keymap("n", "<leader>bj", ":bnext<CR>", opts)
+keymap("n", "<leader>bk", ":bprevious<CR>", opts)
+keymap("n", "<leader>bt", ":BufferLineTogglePin<CR>", opts)
+keymap("n", "<leader>bp", ":BufferLinePick<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
