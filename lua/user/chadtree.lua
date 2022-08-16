3ab06b89286d2657f825f1a20aaa1fc2fd841221
Santiago Garcia <santiago.garcia@posteo.de>
2022-08-16 23:02:20 +0200

    nvim-tree -^> CHADtree

---
+++ lua/user/chadtree.lua
@@ -0,0 +1,10 @@
+local status_ok, chadtree = pcall(require, "chadtree")
+if not status_ok then
+	return
+end
+
+local chadtree_settings = {
+	["view.open_direction"] = "right",
+	["theme.text_colour_set"] = "nord",
+}
+vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
