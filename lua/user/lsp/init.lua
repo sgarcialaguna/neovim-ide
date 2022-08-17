b694fd747b3242ab5fd711f815a6a35a42653f2c
Santiago Garcia <santiago.garcia@posteo.de>
2022-08-17 08:05:23 +0200

    Set TSServer root to Git root for Monorepo

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,4 +1,4 @@
-local status_ok, _ = pcall(require, "lspconfig")
+local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
 	return
 end
@@ -6,3 +6,8 @@ end
 require("user.lsp.lsp-installer")
 require("user.lsp.handlers").setup()
 require("user.lsp.null-ls")
+local util = require("lspconfig.util")
+
+lspconfig.tsserver.setup({
+	root_dir = util.root_pattern(".git"),
+})
