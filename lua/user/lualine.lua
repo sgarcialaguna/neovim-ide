9aa6f45d6b6c847e621d5ec8cc700429760b9887
Santiago Garcia <santiago.garcia@mevis.de>
2022-08-23 12:38:50 +0200

    Reenable lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -42,3 +42,23 @@ local filename = {
 local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
+
+lualine.setup({
+	options = {
+		globalstatus = true,
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
+		lualine_c = { diagnostics, filename },
+		lualine_x = { diff, spaces, "encoding", "fileformat", filetype },
+		lualine_y = { location },
+		lualine_z = { "progress" },
+	},
+})
