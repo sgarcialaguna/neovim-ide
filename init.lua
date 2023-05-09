ab43b16a12610a2a27c8129bd20400c49394e6ce
Santiago Garcia <santiago.garcia@posteo.de>
2023-05-09 23:02:59 +0200

    LSP-Zero

--- init.lua
+++ init.lua
@@ -3,3 +3,7 @@ require("user.keymaps")
 require("user.plugins")
 require("user.autocommands")
 require("user.leap")
+if not vim.g.vscode then
+    require("user.lsp")
+end
+
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -39,4 +39,30 @@ lazy.setup({
 
 	-- Colorschemes
 	{ "folke/tokyonight.nvim", enabled = not vim.g.vscode, config = function() vim.cmd [[colorscheme tokyonight]] end },
+
+    -- LSP
+    {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  dependencies = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      build = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+    --'hrsh7th/cmp-nvim-lsp-signature-help'
+    'ray-x/lsp_signature.nvim'
+
+  },
+  enabled = not vim.g.vscode
+}
 })
